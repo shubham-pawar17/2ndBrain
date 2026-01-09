03d41515aaf3744ca5542920a8272e63e4bd50fa
shubham <pawarshubham5959@gmail.com>
2026-01-10 00:00:45 +0530

    Remove root backend files, keep backend and frontend separated

--- src/config.ts
+++
@@ -1,8 +0,0 @@
-export const JWT_PASSWORD ="Shubhampawar1";
-import express from "express";
-
-// declare module "express-serve-static-core" {
-//   interface Request {
-//     userId?: string; // or the correct type for your userId (string, number, etc)
-//   }
-// }
